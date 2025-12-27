57c637a6d04f2074a49b13d8cc6d42e5d6c5e27a
EkVairagi <sangamnayak@gmail.com>
2025-12-27 21:57:02 +0530

    chore: enable maven publishing for jitpack

--- timeago-parser/build.gradle.kts
+++ timeago-parser/build.gradle.kts
@@ -3,6 +3,27 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = rootProject.group
+version = rootProject.version
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            groupId = "com.github.EkVairagi"
+            artifactId = "timeago-parser"
+            version = rootProject.version.toString()
+        }
+    }
+}
+
 dependencies {
     implementation(libs.google.jsr305)
 }
